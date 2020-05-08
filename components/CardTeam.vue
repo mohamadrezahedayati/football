92785b8e44c54ef30408639eaed1777a7f646762
hedayati.m <hedayati.m.orchid@gmail.com>
2020-05-08 15:59:32 +0430

    custom loader

--- components/CardTeam.vue
+++ components/CardTeam.vue
@@ -1,9 +1,17 @@
 <template>
     <div class="container-team-card">
-        <nuxt-link to="">
-            <b-card :title="team.name" :img-src="getImg()" img-width="200px" img-alt="Image" img-top tag="article" class="mb-2">
+        <nuxt-link :to="'team/' + team.id">
+            <b-card :title="team.name" :img-src="getImg()" img-alt="Image" img-top tag="article" class="mb-2">
             <b-card-text>
-                Some quick example text to build on the card title and make up the bulk of the card's content.
+                <div>
+                    <span>short name : </span>
+                    <span>{{team.shortName}}</span>
+                </div>
+                <hr>
+                <div>
+                    <span>Area:</span>
+                    <span>{{team.area.name}}</span>
+                </div>
             </b-card-text>
             </b-card>
         </nuxt-link>
@@ -18,15 +26,7 @@
         methods: {
             getImg(){
                 if(this.team.crestUrl != null){
-                    var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-                    var http = new XMLHttpRequest();
-                    http.open('HEAD',this.team.crestUrl, false);
-                    http.send();
-                    if(http.status =='404') {
-                        return '/team.jpg'
-                    } else {
-                        return this.team.crestUrl
-                    }
+                    return this.team.crestUrl
                 } else {
                     return '/team.jpg'
                 }
@@ -35,6 +35,9 @@
     }
 </script>
 
-<style lang="scss" scoped>
-
+<style scoped>
+    .container-team-card img{
+        margin: 0 auto;
+        width: 40% !important;
+    }
 </style>
